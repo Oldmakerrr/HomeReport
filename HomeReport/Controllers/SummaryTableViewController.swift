babf8947acd2fa308efaf2ae226a80c54952f50d
V <V>
2022-08-09 23:05:39 +0400

    add aggregate functions

--- HomeReport/Controllers/SummaryTableViewController.swift
+++ HomeReport/Controllers/SummaryTableViewController.swift
@@ -10,6 +10,7 @@ import CoreData
 
 class SummaryTableViewController: UITableViewController {
     
+    //MARK: - Outlets
     @IBOutlet weak var totalSalesDollarLabel: UILabel!
     @IBOutlet weak var numCondoSoldLabel: UILabel!
     @IBOutlet weak var numSFSoldLabel: UILabel!
@@ -18,30 +19,47 @@ class SummaryTableViewController: UITableViewController {
     @IBOutlet weak var avgPriceCondoLabel: UILabel!
     @IBOutlet weak var avgPriceSFLabel: UINavigationItem!
     
-    var managedObjectContext: NSManagedObjectContext?
+    //MARK: - properties
+    
+    var home: Home?
+    
+    var managedObjectContext: NSManagedObjectContext! {
+        didSet { home = Home(context: managedObjectContext) }
+    }
+    
+    private var soldPredicate: NSPredicate!
+    
+    //MARK: - lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTextToLabel()
+    }
     
+    private func setTextToLabel() {
+        guard let home = home else { return }
+        totalSalesDollarLabel.text = home.totalSoldHomesValue(moc: managedObjectContext).currencyFormatter
+        numCondoSoldLabel.text = String(home.totalSoldCondo(moc: managedObjectContext))
+        numSFSoldLabel.text = String(home.totalSoldSingleFamilyHome(moc: managedObjectContext))
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        var rowCount = 0
+        switch section {
+        case 0:
+            rowCount = 3
+        case 1, 2:
+            rowCount = 2
+        default:
+            break
+        }
+        return rowCount
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
 }
--- HomeReport/Entity/Home+CoreDataClass.swift
+++ HomeReport/Entity/Home+CoreDataClass.swift
@@ -9,7 +9,59 @@
 import Foundation
 import CoreData
 
+enum HomeType: String {
+    case condo = "Condo"
+    case singleFamily = "Single Family"
+}
+
 @objc(Home)
 public class Home: NSManagedObject {
 
+    private let isForSalePredicate = NSPredicate(format: "isForSale = false")
+    private let request: NSFetchRequest<Home> = Home.fetchRequest()
+    
+    func totalSoldHomesValue(moc: NSManagedObjectContext) -> Double {
+        request.predicate = isForSalePredicate
+        request.resultType = .dictionaryResultType
+        
+        let sumExpressionDescription = NSExpressionDescription()
+        sumExpressionDescription.name = "totalSales"
+        sumExpressionDescription.expression = NSExpression(forFunction: "sum:", arguments: [NSExpression(forKeyPath: "price")])
+        sumExpressionDescription.resultType = .double
+        
+        request.propertiesToFetch = [sumExpressionDescription]
+        
+        if let result = try? moc.fetch(request as! NSFetchRequest<NSFetchRequestResult>) as? [NSDictionary],
+           let totalSales = result.first?["totalSales"] as? Double {
+            return totalSales
+        }
+        return 0
+    }
+    
+    func totalSoldCondo(moc: NSManagedObjectContext) -> Int {
+        let typePredicate = NSPredicate(format: "homeType = '\(HomeType.condo.rawValue)'")
+        let predicate = NSCompoundPredicate(type: .and, subpredicates: [isForSalePredicate, typePredicate])
+        
+        request.resultType = .countResultType
+        request.predicate = predicate
+        
+        if let result = try? moc.fetch(request as! NSFetchRequest<NSFetchRequestResult>) as? [Int],
+           let count = result.first {
+           return count
+        }
+        return 0
+    }
+    
+    func totalSoldSingleFamilyHome(moc: NSManagedObjectContext) -> Int {
+        let typePredicate = NSPredicate(format: "homeType = '\(HomeType.singleFamily.rawValue)'")
+        let predicate = NSCompoundPredicate(type: .and, subpredicates: [isForSalePredicate, typePredicate])
+        
+        request.predicate = predicate
+        
+        if let count = try? moc.count(for: request), count != NSNotFound {
+            return count
+        }
+        return 0
+    }
+    
 }
